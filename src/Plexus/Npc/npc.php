d7556ecc81f749f5651414af9902518f0f676787
Zero <epicsteve33@gmail.com>
2017-10-10 11:54:25 -0700

    Update npc.php

--- src/Plexus/Npc/npc.php
+++ src/Plexus/Npc/npc.php
@@ -3,5 +3,15 @@
 namespace Npc;
 
 class npc {
+  
+  public $npc = [];
 
+  public function createNpc($eid, $x, $y, $z, $yaw, $pitch){
+  //TODO
+  }
+  
+  public function deleteNpc($eid){
+  //TODO
+  }
+  
 }
